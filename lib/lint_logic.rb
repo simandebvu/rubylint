a182af43304b612c53d3e7ed949fb7c7fb005d30
Shingirayi Mandebvu <shingim1@gmail.com>
2020-06-25 19:22:44 +0200

    Pass the test for more than 1 line between bodies

--- lib/lint_logic.rb
+++ lib/lint_logic.rb
@@ -1,34 +1,7 @@
 class LintLogic
     $errors_found = Hash.new
-    KeywordExpStarters = [
-        /^module\b/,
-        /^class\b/,
-        /^if\b/,
-        /(=\s*|^)until\b/,
-        /(=\s*|^)for\b/,
-        /^unless\b/,
-        /def/,
-        /\bdo\b/,
-        /^else\b/,
-        /^elsif\b/,
-        /\bwhen\b/,
-        /\{[^\}]*$/,
-        /\[[^\]]*$/
-     ]
 
-     KeywordExpClosers = [
-        /^rescue\b/,
-        /^ensure\b/,
-        /^elsif\b/,
-        /^end\b/,
-        /end/,
-        /^else\b/,
-        /\bwhen\b/,
-        /^[^\{]*\}/,
-        /^[^\[]*\]/
-     ]
-
-    def get_line_length(content)
+    def check_line_length(content)
         content.each_with_index do |stringScan,idx|
             line = stringScan.string.strip!
             next if line.nil? || stringScan.nil? 
@@ -39,67 +12,32 @@ class LintLogic
         return $errors_found
     end
 
-    def look_for_termination(content)
-        filter_criteria = ["Unnecessary character"]
+    def check_termination(content)
+        filter_criteria = ["Unnecessary character ';'"]
         content.each_with_index do |stringScan,idx|  
             next if stringScan.nil?
             if stringScan.exist?(/;/)
-                $errors_found[idx] = "Unnecessary character"  
+                $errors_found[idx] = "Unnecessary character ';'"  
             end            
         end
         return $errors_found.select{|k, v| filter_criteria.include?(v) }
     end
 
-    def get_indentation(content)
-        lines_to_check = {}
-        check_indentation_level(content)
-        content.each_with_index do |stringScan,idx| 
-            spaces = stringScan.scan(/\s+/)
-            next unless !spaces.nil?
-            if spaces.length > 2
-                lines_to_check['Indentation'] = idx + 1
-            end  
-        end
-        return lines_to_check
-    end
-
-    def check_indentation_level(content)
-        levels = []
-        level = 0
-        content.each_with_index do |stringScan, idx|
-            stringScan.reset
-            levels << level
-            KeywordExpStarters.each_with_index do |re, idx|
-                next unless !stringScan.nil?
-                level += 1 if stringScan.exist?(re)
-                p idx
-                next unless stringScan.exist?(KeywordExpClosers[idx])
-                level -= 1
-                levels[i] = level
+    def check_row_spacing(content)
+        filter_criteria = ["Two or more empty lines"]
+        content.each_with_index do |stringScan,idx|
+            next if stringScan.nil?
+            if stringScan.scan(/\n/)
+                if content[idx+1].scan(/\n/)
+                    # p "Two lines at #{idx+1}"
+                    $errors_found[idx] = "Two or more empty lines"
+                end
             end
-      
-            # levels << level
-            # stringScan.reset
-            # KeywordExpStarters.each do |re|
-            #     s = stringScan.scan_until(re)
-            #     next unless !s.nil?
-            #     u = stringScan.string()
-            #     p u
-            #     level += 1 
-            #     KeywordExpClosers.each do |r|
-            #         u = stringScan.string()
-            #         y = stringScan.scan_until(r)
-            #         p u
-            #         next unless !y.nil?
-            #         level -= 1 
-            #     end
-            #     break
-            # end
         end
-        p levels
-        return levels
+        return $errors_found.select{|k, v| filter_criteria.include?(v) }
     end
 
+    
     def log_error(type, line)
         puts "#{type} error. Line : #{line}."
     end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -4,38 +4,30 @@ require_relative "./spec_tests/spaces.rb"
 require_relative "../lib/filereader.rb"
 require_relative "../lib/lint_logic.rb"
 
-# describe "#get_indentation" do
-#     let(:test_file){'spec/spec_tests/indentation-wrong.rb'}
-#     let(:file_scanned){ FileReader.new(test_file)}
-#     let(:logic_class){ LintLogic.new}
-#     it "Must correctly detect wrong indentation." do
-#         expect(logic_class.get_indentation(file_scanned.file_contents)            
-#         ).to eql({'Indentation'=>2})
-#     end
-# end
+
 describe LintLogic do
     let(:logic_class){ LintLogic.new}
-    describe "#get_line_length" do 
+    describe "#check_line_length" do 
         let(:test_file){'spec/spec_tests/line_length.rb'}
         let(:file_buffer){ FileReader.new(test_file)}
         it "Must advise user on long lines greater than 80." do
-            expect(logic_class.get_line_length(file_buffer.file_contents)).to eql({"Line length"=>2})
+            expect(logic_class.check_line_length(file_buffer.file_contents)).to eql({"Line length"=>2})
         end
     end
 
-    describe "#look_for_termination" do 
+    describe "#check_termination" do 
         let(:test_file){'spec/spec_tests/termination.rb'}
         let(:file_buffer){ FileReader.new(test_file)}
         it "Must advise user on unnecessary termination colon." do
-            expect(logic_class.look_for_termination(file_buffer.file_contents)).to eql({0=>"Unnecessary character", 2=>"Unnecessary character"})
+            expect(logic_class.check_termination(file_buffer.file_contents)).to eql({0=>"Unnecessary character ';'", 2=>"Unnecessary character ';'"})
         end
     end
 
-describe "#checkspaces" do 
-    let(:test_file){'./spec_tests/spaces.rb'}
-    let(:file_buffer){ FileReader.new(test_file)} 
-    it "Must check spaces around operators." do
-        expect(spaces?(test_file)).to eql(false)
-    end   
-end
+    describe "#check_row_spacing" do 
+        let(:test_file){'spec/spec_tests/spaces.rb'}
+        let(:file_buffer){ FileReader.new(test_file)} 
+        it "Must check spaces around operators." do
+            expect(logic_class.check_row_spacing(file_buffer.file_contents)).to eql({4=>"Two or more empty lines"})
+        end   
+    end
 end
